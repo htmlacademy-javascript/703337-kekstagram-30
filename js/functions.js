86ab5675babe241f960da3dd893f5bd7f500eba6
Татьяна <lee-tanya2insp@yandex.ru>
2023-10-14 14:16:24 +0300

    Исправления согласно замечаниям

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,9 @@
 function checkLengthString (str, maxLength){
-  if(str.length <= maxLength){
-    return true;
-  }
-  return false;
+  return (str.length <= maxLength);
 }
-checkLengthString('Hello, world!', 10);
 
 function checkPalindrom (str){
   const normalizeString = str.replaceAll(' ', '').toUpperCase();
-  let newString = '';
-  for(let i = normalizeString.length - 1; i >= 0; i--){
-    newString += normalizeString[i];
-  }
+  const newString = normalizeString.split('').reverse().join('');
   return (normalizeString === newString);
 }
-
